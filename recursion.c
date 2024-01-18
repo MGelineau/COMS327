47079415c7b5a1ddc4a6fb040656ca1ed0ca2318
MGelineau <gelineau1234321@gmail.com>
2024-01-18 13:46:20 -0600

    fib recursion is a bad alg

--- recursion.c
+++ recursion.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+unsigned fib(unsigned i)
+{
+    if (i < 2)
+    {
+        return i;
+    }
+
+    return fib(i - 1) + fib(i - 2);
+}
 
 // MAN pages?
 int main(int argc, char *argv[])
 {
+    printf("%d\n", fib(atoi(argv[1])));
 
+    return 0;
 }
+
+/*
+ *gcc recursion.c -o recursion
+ *./recursion 1,2,3 ...etc
+ */
