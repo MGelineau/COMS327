ee0dea7705244fe7c4948454c09cc36a393611b5
MGelineau <gelineau1234321@gmail.com>
2024-02-13 12:54:39 -0600

    stuff

--- Week_4/string.c
+++ Week_4/string.c
@@ -9,10 +9,10 @@ size_t strlen327(const char *s)
     return i;
 }
 
-
 int strcmp_idiomatic(const char *s1, const char *s2)
 {
-    while(*s1 && *s1++ == *s2++);
+    while (*s1 && *s1++ == *s2++)
+        ;
     return *s1 - *s2;
 }
 
@@ -20,8 +20,9 @@ int strcomp327(const char *s1, const char *s2)
 {
     int i;
 
-    for(i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++);
-    
+    for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
+        ;
+
     return s1[i] - s2[i];
 }
 
@@ -43,5 +44,21 @@ int main(int argc, char *argv[])
     a[0] = 'h';
     printf("%s\n", a);
 
+    printf("%s\n", &a[0]);
+    printf("%s\n", a + 4);
+
     return 0;
 }
+
+/*
+*   Pointer arithmetic -
+    int *a;
+    int i;
+
+    a[i] == *(a + i)
+    &a[i] == a + i
+
+    i is an index, not an offset.  The compiler automatically converted to an offset for us:
+        We write (a + i), compiler translate to (a + (4 * i))
+                                                Because ints are 4 bytes
+*/
